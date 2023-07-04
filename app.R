dbd0ebad3671d27feafcfe6a0e60b80b1627d100
Talia261201 <128403783+Talia261201@users.noreply.github.com>
2023-07-04 11:31:05 +0700

    Add files via upload

---
+++ app.R
@@ -0,0 +1,64 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  selectInput("Dataset", label = "Dataset", choices = ls("packages:dataset")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
+
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output,session) {
+  output4summary <- renderPrint({
+    dataset <- get(input$dataset, "packages:dataset")
+    summary(dataset)
+  })
+  
+
+    output$table <- renderTable({
+        # generate bins based on input$bins from ui.R
+        dataset <- get(input$dataset, "packages:dataset")
+        dataset
+        })
+}
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
